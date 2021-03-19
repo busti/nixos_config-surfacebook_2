bbb3b98c5ad78024c4fe1a350fb49442f2a4309f
busti <mbust@live.de>
2021-03-19 20:51:27 +0100

    configure hardware and cryptlvm grub

--- configuration.nix
+++ configuration.nix
@@ -14,8 +14,22 @@
   nixpkgs.config.allowUnfree = true;
 
   # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
+  # boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub = {
+    enable = true;
+    version = 2;
+    device = "nodev";
+    efiSupport = true;
+    enableCryptodisk = true;
+  };
+  boot.initrd.luks.devices = {
+    root = {
+      device = "/dev/disk/by-label/cryptroot";
+      preLVM = true;
+    };
+  };
+
 
   # networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
