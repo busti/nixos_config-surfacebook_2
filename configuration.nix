1e276b9bfe8a868a2cae5540641742731eafe247
busti <oss@busti.cool>
2021-04-30 00:57:23 +0200

    move home monitor setup to workspace config

--- configuration.nix
+++ configuration.nix
@@ -98,16 +98,6 @@ in
     };
     displayManager = {
       defaultSession = "none+i3";
-      sessionCommands = let xrandr = "${lib.getBin pkgs.xorg.xrandr}/bin/xrandr"; in ''
-        ${xrandr} --setprovideroutputsource 1 0
-        ${xrandr} --setprovideroutputsource 2 0
-        ${xrandr} --output DVI-I-2-1 --right-of eDP1 --mode 1680x1050
-        ${xrandr} --newmode "lggarbage" 138.50 1920 1968 2000 2080  1080 1083 1088 1111 +hsync -vsync
-        ${xrandr} --addmode DP1-1 lggarbage
-        ${xrandr} --output DP1-1 --mode lggarbage --right-of DVI-I-2-1
-        ${xrandr} --output DP1-2 --above DP1-1
-        ${xrandr} --output DVI-I-3-2 --right-of DP1-1 --mode 1600x1200
-      '';
     };
     windowManager.i3 = {
       enable = true;
