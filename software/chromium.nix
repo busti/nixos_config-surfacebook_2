2fc7fb214b49b5cabb560719c2680f1de3e97c8a
busti <oss@busti.cool>
2021-05-01 00:58:26 +0200

    add chrome extensions

--- software/chromium.nix
+++ software/chromium.nix
@@ -5,5 +5,11 @@
     ungoogled-chromium
   ];
 
-  
+  programs.chromium = {
+    enable = true;
+    extensions = [
+      "ocaahdebbfolfmndjeplogmgcagdmblk;https://raw.githubusercontent.com/NeverDecaf/chromium-web-store/master/updates.xml"
+      "cjpalhdlnbpafiamejdnhcphjbkeiagm" # ublock origin
+    ];
+  };
 }
