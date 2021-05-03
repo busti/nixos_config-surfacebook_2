8e71cc6d5f76d896bbbfe13eea577fb55979671f
busti <oss@busti.cool>
2021-05-03 03:59:34 +0200

    move graphics drivers to desktop env modules

--- desktop/i3wm/i3wm.nix
+++ desktop/i3wm/i3wm.nix
@@ -3,7 +3,32 @@
 {
   services.xserver = {
     enable = true;
+    videoDrivers = [ "intel" "nvidia" "displaylink" ];
     exportConfiguration = true;
+    deviceSection = ''
+      Option "DRI" "3"
+      Option "TearFree" "true"
+      Option "PageFlip" "off"
+      Option "RandRRotation" "True"
+    '';
+    extraConfig = ''
+      Section "OutputClass"
+        Identifier "DLRot"
+        MatchDriver "evdi"
+        Option "PageFlip" "off"
+      EndSection
+    '';
+    displayManager.sessionCommands = let xrandr = "${lib.getBin pkgs.xorg.xrandr}/bin/xrandr"; in ''
+      ${xrandr} --setprovideroutputsource 1 0
+      ${xrandr} --setprovideroutputsource 2 0
+      ${xrandr} --output DVI-I-2-1 --right-of eDP1 --mode 1680x1050
+      ${xrandr} --newmode "lggarbage" 138.50 1920 1968 2000 2080  1080 1083 1088 1111 +hsync -vsync
+      ${xrandr} --addmode DP1-1 lggarbage
+      ${xrandr} --output DP1-1 --mode lggarbage --right-of DVI-I-2-1
+      ${xrandr} --output DP1-2 --above DP1-1
+      ${xrandr} --output DVI-I-3-2 --right-of DP1-1 --mode 1600x1200
+    '';
+    libinput.enable = true;
     desktopManager = {
       xterm.enable = false;
     };
--- hardware/surfacebook_2.nix
+++ hardware/surfacebook_2.nix
@@ -5,31 +5,19 @@
     "${builtins.fetchGit { url = "https://github.com/NixOS/nixos-hardware.git"; }}/microsoft/surface"
   ];
 
-  # video
-  services.xserver = {
-    videoDrivers = [ "intel" "nvidia" ];
-    libinput.enable = true;
-    deviceSection = ''
-      Option "DRI" "3"
-      Option "TearFree" "true"
-      Option "PageFlip" "off"
-      Option "RandRRotation" "True"
-    '';
-  };
-
   nixpkgs.config.packageOverrides = pkgs: {
     vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
   };
 
-  hardware.opengl = {
-    enable = true;
-    extraPackages = with pkgs; [
-      intel-media-driver # LIBVA_DRIVER_NAME=iHD
-      vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-      vaapiVdpau
-      libvdpau-va-gl
-    ];
-  };
+  #hardware.opengl = {
+  #  enable = true;
+  #  extraPackages = with pkgs; [
+  #    intel-media-driver # LIBVA_DRIVER_NAME=iHD
+  #    vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+  #    vaapiVdpau
+  #    libvdpau-va-gl
+  #  ];
+  #};
 
   boot.kernelParams = [ "i915.enable_psr=0" ];
 
