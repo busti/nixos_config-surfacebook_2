6c0e697869feafafc1dfbc62eae63f0a69d1086f
busti <oss@busti.cool>
2021-05-07 19:13:58 +0200

    disable certain chrome features

--- software/chromium.nix
+++ software/chromium.nix
@@ -8,8 +8,17 @@
   programs.chromium = {
     enable = true;
     extensions = [
-      "ocaahdebbfolfmndjeplogmgcagdmblk;https://raw.githubusercontent.com/NeverDecaf/chromium-web-store/master/updates.xml"
+      # "ocaahdebbfolfmndjeplogmgcagdmblk;https://raw.githubusercontent.com/NeverDecaf/chromium-web-store/master/updates.xml"
       "cjpalhdlnbpafiamejdnhcphjbkeiagm" # ublock origin
     ];
+    extraOpts = {
+      "BrowserSignin" = 0;
+      "SyncDisabled" = true;
+      "PasswordManagerEnabled" = false;
+      "SpellcheckEnabled" = true;
+      "SpellcheckLanguage" = [
+        "de" "en-US"
+      ];
+    };
   };
 }
