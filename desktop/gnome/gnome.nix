5626c2d278c360686cb0602ff1133284ee419f65
busti <oss@busti.cool>
2021-05-15 17:21:45 +0200

    wip

--- desktop/gnome/gnome.nix
+++ desktop/gnome/gnome.nix
@@ -9,6 +9,13 @@
     };
   };
 
+  environment.gnome3.excludePackages = with pkgs.gnome3; [
+    baobab cheese eog epiphany gedit simple-scan totem yelp
+    gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-contacts
+    gnome-font-viewer gnome-logs gnome-maps gnome-music gnome-photos gnome-screenshot
+    gnome-system-monitor gnome-weather
+  ];
+
   environment.systemPackages = with pkgs; [ gnomeExtensions.appindicator ];
   services.udev.packages = with pkgs; [ gnome3.gnome-settings-daemon ];
 }
