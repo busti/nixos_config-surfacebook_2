c9e290e32922f4044bb2aff40ebf0f6e9213b0b4
busti <oss@busti.cool>
2021-06-08 02:02:20 +0200

    annotate gnome crap

--- config/desktop/env/gnome/gnome.nix
+++ config/desktop/env/gnome/gnome.nix
@@ -16,7 +16,18 @@
   };
 
   environment.gnome3.excludePackages = with pkgs.gnome; [
-    baobab cheese eog epiphany gedit simple-scan totem yelp evince file-roller geary seahorse
+    baobab      # disk usage analyzer
+    cheese      # photo booth
+    eog         # image viewer
+    epiphany    # web browser
+    gedit       # text editor
+    simple-scan # document scanner
+    totem       # video player
+    yelp        # help viewer
+    evince      # document viewer
+    file-roller # archive manager
+    geary       # email client
+    seahorse    # password manager
     gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-contacts
     gnome-font-viewer gnome-logs gnome-maps gnome-music gnome-photos gnome-screenshot
     gnome-system-monitor gnome-weather gnome-disk-utility pkgs.gnome-connections
