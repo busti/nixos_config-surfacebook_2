3d3823346389ee9d2463f20f37c622857244d93d
busti <oss@busti.cool>
2021-06-29 00:15:50 +0200

    install nvtop

--- config/hardware/surfacebook_2.nix
+++ config/hardware/surfacebook_2.nix
@@ -30,7 +30,7 @@ in {
     };
   };
 
-  environment.systemPackages = [ nvidia-offload ];
+  environment.systemPackages = [ nvidia-offload nvtop ];
 
   nixpkgs.config.packageOverrides = pkgs: {
     vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
