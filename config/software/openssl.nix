c03bdd2e6e14002d39eaf3c1cfffe720dbbd6ecd
busti <oss@busti.cool>
2021-08-07 21:12:26 +0200

    fix missing string conversion

--- config/software/openssl.nix
+++ config/software/openssl.nix
@@ -2,7 +2,7 @@
   environment = {
     systemPackages = with pkgs; [ openssl ];
     variables = with pkgs; {
-      OPENSSL_DIR = openssl.dev;
+      OPENSSL_DIR = "${openssl.dev}";
       OPENSSL_LIB_DIR = "${openssl.out}/lib";
     };
   };
